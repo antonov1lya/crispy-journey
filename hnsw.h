c65ff1b8a1b46530206243c3cc2a0cfd014a1493
antonov1lya <a222i@ya.ru>
2025-01-25 18:46:30 +0300

    add heuristic

--- hnsw.h
+++ hnsw.h
@@ -18,13 +18,16 @@ template <typename Space>
 struct HNSW
 {
     HNSW(size_t M, size_t ef_construction, size_t max_elements) : M_{M}, maxM_{M}, maxM0_{2 * M},
-                                             ef_construction_{ef_construction}, max_elements_{max_elements}
+                                                                  ef_construction_{ef_construction}, max_elements_{max_elements}
     {
         was_ = std::vector<size_t>(max_elements_);
     }
     void Add(const Point &point, int level);
-    std::vector<size_t> SearchLayer(size_t query, size_t enter_point, size_t ef, size_t level);
-    std::vector<size_t> SelectNeighboursSimple(size_t query, std::vector<size_t> candidates, size_t M);
+    std::priority_queue<std::pair<float, size_t>,
+                        std::vector<std::pair<float, size_t>>,
+                        std::less<std::pair<float, size_t>>>
+    SearchLayer(size_t query, size_t enter_point, size_t ef, size_t level);
+    std::vector<size_t> SelectNeighbours(size_t query, std::priority_queue<std::pair<float, size_t>, std::vector<std::pair<float, size_t>>, std::less<std::pair<float, size_t>>> &candidates, size_t M);
     std::vector<size_t> Search(size_t query, size_t K, size_t ef);
     size_t M_;
     size_t maxM_;
@@ -36,7 +39,7 @@ struct HNSW
     std::vector<Point> data_;
     std::vector<Layer> layers_;
     Space space_;
-    std::vector<size_t>was_;
+    std::vector<size_t> was_;
     size_t current_was_;
     size_t max_elements_;
 };
@@ -60,7 +63,7 @@ void HNSW<Space>::Add(const Point &point, int level)
     size_t enter_point = enter_points_[max_level_];
     for (int i = max_level_; i > level; --i)
     {
-        enter_point = SearchLayer(index, enter_point, 1, i)[0];
+        enter_point = SearchLayer(index, enter_point, 1, i).top().second;
     }
     for (int i = std::min(max_level_, level); i >= 0; --i)
     {
@@ -73,13 +76,10 @@ void HNSW<Space>::Add(const Point &point, int level)
             }
         }
         auto nearest_neighbors = SearchLayer(index, enter_point, ef_construction_, i);
-        enter_point = nearest_neighbors[0];
-        if (nearest_neighbors.size() > M_)
-        {
-            nearest_neighbors.resize(M_);
-        }
-        layers_[i].graph_[index] = nearest_neighbors;
-        for (size_t next : nearest_neighbors)
+        layers_[i].graph_[index] = SelectNeighbours(index, nearest_neighbors, M_);
+        enter_point = layers_[i].graph_[index][0];
+
+        for (size_t next : layers_[i].graph_[index])
         {
             layers_[i].graph_[next].push_back(index);
             size_t maxM = maxM_;
@@ -89,29 +89,40 @@ void HNSW<Space>::Add(const Point &point, int level)
             }
             if (layers_[i].graph_[next].size() > maxM)
             {
-                layers_[i].graph_[next] = SelectNeighboursSimple(next, layers_[i].graph_[next], maxM);
+                std::priority_queue<std::pair<float, size_t>,
+                                    std::vector<std::pair<float, size_t>>,
+                                    std::less<std::pair<float, size_t>>>
+                    queue;
+                for (size_t neighbour : layers_[i].graph_[next])
+                {
+                    queue.emplace(space_.Distance(data_[next], data_[neighbour]), neighbour);
+                }
+                layers_[i].graph_[next] = SelectNeighbours(next, queue, maxM);
             }
         }
     }
 }
 
 template <typename Space>
-inline std::vector<size_t> HNSW<Space>::SearchLayer(size_t query, size_t enter_point, size_t ef, size_t level)
+inline std::priority_queue<std::pair<float, size_t>,
+                           std::vector<std::pair<float, size_t>>,
+                           std::less<std::pair<float, size_t>>>
+HNSW<Space>::SearchLayer(size_t query, size_t enter_point, size_t ef, size_t level)
 {
     Layer &layer = layers_[level];
     ++current_was_;
-    was_[enter_point]=current_was_;
+    was_[enter_point] = current_was_;
     std::priority_queue<std::pair<float, size_t>,
                         std::vector<std::pair<float, size_t>>,
                         std::greater<std::pair<float, size_t>>>
         candidates;
-    candidates.emplace(space_.Distance(data_[enter_point], data_[query]), enter_point);
+    FloatType enter_point_distance = space_.Distance(data_[enter_point], data_[query]);
+    candidates.emplace(enter_point_distance, enter_point);
     std::priority_queue<std::pair<float, size_t>,
                         std::vector<std::pair<float, size_t>>,
                         std::less<std::pair<float, size_t>>>
         nearest_neighbours;
-    nearest_neighbours.emplace(space_.Distance(data_[enter_point], data_[query]), enter_point);
-
+    nearest_neighbours.emplace(enter_point_distance, enter_point);
     while (!candidates.empty())
     {
         size_t current = candidates.top().second;
@@ -123,9 +134,9 @@ inline std::vector<size_t> HNSW<Space>::SearchLayer(size_t query, size_t enter_p
         }
         for (size_t next : layer.graph_[current])
         {
-            if (was_[next]!=current_was_)
+            if (was_[next] != current_was_)
             {
-                was_[next]=current_was_;
+                was_[next] = current_was_;
                 furthest_distance = nearest_neighbours.top().first;
                 FloatType distance = space_.Distance(data_[next], data_[query]);
                 if (distance < furthest_distance or nearest_neighbours.size() < ef)
@@ -140,33 +151,44 @@ inline std::vector<size_t> HNSW<Space>::SearchLayer(size_t query, size_t enter_p
             }
         }
     }
-    std::vector<size_t> nearest_neighbours_vector;
-    while (!nearest_neighbours.empty())
-    {
-        nearest_neighbours_vector.push_back(nearest_neighbours.top().second);
-        nearest_neighbours.pop();
-    }
-    std::reverse(nearest_neighbours_vector.begin(), nearest_neighbours_vector.end());
-    return nearest_neighbours_vector;
+    return nearest_neighbours;
 }
 
 template <typename Space>
-inline std::vector<size_t> HNSW<Space>::SelectNeighboursSimple(size_t query, std::vector<size_t> candidates, size_t M)
+inline std::vector<size_t> HNSW<Space>::SelectNeighbours(size_t query, std::priority_queue<std::pair<float, size_t>, std::vector<std::pair<float, size_t>>, std::less<std::pair<float, size_t>>> &candidates, size_t M)
 {
-    std::vector<std::pair<FloatType, size_t>> array;
-    array.reserve(candidates.size());
-    for (size_t i = 0; i < candidates.size(); ++i)
+    std::vector<std::pair<float, size_t>> queue;
+    queue.reserve(candidates.size());
+    while (!candidates.empty())
     {
-        array.emplace_back(space_.Distance(data_[query], data_[candidates[i]]), candidates[i]);
+        queue.push_back(candidates.top());
+        candidates.pop();
     }
-    std::sort(array.begin(), array.end());
-    std::vector<size_t> nearest_neighbours;
-    nearest_neighbours.reserve(candidates.size());
-    for (size_t i = 0; i < std::min(M, array.size()); ++i)
+    std::reverse(queue.begin(), queue.end());
+    std::vector<size_t> selected;
+    selected.reserve(M);
+    for (auto &[dist, element] : queue)
     {
-        nearest_neighbours.push_back(array[i].second);
+        if (selected.size() >= M)
+        {
+            break;
+        }
+        bool good = true;
+        for (size_t neighbour : selected)
+        {
+            FloatType curdist = space_.Distance(data_[element], data_[neighbour]);
+            if (curdist < dist)
+            {
+                good = false;
+            }
+        }
+        if (good)
+        {
+            selected.push_back(element);
+        }
     }
-    return nearest_neighbours;
+
+    return selected;
 }
 
 template <typename Space>
@@ -175,12 +197,20 @@ inline std::vector<size_t> HNSW<Space>::Search(size_t query, size_t K, size_t ef
     size_t enter_point = enter_points_[max_level_];
     for (size_t i = max_level_; i >= 1; --i)
     {
-        enter_point = SearchLayer(query, enter_point, 1, i)[0];
+        enter_point = SearchLayer(query, enter_point, 1, i).top().second;
     }
     auto nearest_neighbours = SearchLayer(query, enter_point, ef, 0);
-    if (nearest_neighbours.size() > K)
+    while (nearest_neighbours.size() > K)
     {
-        nearest_neighbours.resize(K);
+        nearest_neighbours.pop();
     }
-    return nearest_neighbours;
+    std::vector<size_t> array;
+    array.reserve(nearest_neighbours.size());
+    while (!nearest_neighbours.empty())
+    {
+        array.push_back(nearest_neighbours.top().second);
+        nearest_neighbours.pop();
+    }
+    std::reverse(array.begin(), array.end());
+    return array;
 }
