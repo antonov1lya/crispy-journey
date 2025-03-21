1060c415408ac1644491561a6f673303feef29c7
antonov1lya <a222i@ya.ru>
2025-03-21 22:30:18 +0300

    added new version

--- hnsw.h
+++ hnsw.h
@@ -42,9 +42,15 @@ struct HNSW
     HNSW(std::ifstream &file);
     void Add(const Point &point, int level);
     QueueLess SearchLayer(Point &query, size_t enter_point, size_t ef, size_t level);
+    QueueLess SearchLayer0(Point &query, size_t enter_point, size_t ef, size_t level);
     std::vector<size_t> SelectNeighbours(QueueLess &candidates, size_t M, size_t maxM);
     std::vector<size_t> Search(Point &query, size_t K, size_t ef);
     std::vector<size_t> SSG(size_t node, std::vector<size_t> &candidates, size_t M);
+
+    void MemoryManager();
+    void AddNeighborhood(size_t i, size_t j);
+    void AddNeighborhood(size_t i);
+
     void Improve();
     void Save(std::ofstream &file, size_t precision = 1);
     size_t M_;
@@ -59,8 +65,13 @@ struct HNSW
     std::vector<Point> data_;
     std::vector<size_t> was_;
     std::vector<Node> graph_;
+
+    std::vector<size_t> A0_;
+    std::vector<size_t> B0_;
+    size_t size0_ = 0;
+
     Space space_;
-    float ssg_cos=0.5;
+    float ssg_cos = 0.5;
 };
 
 template <typename Space>
@@ -126,6 +137,48 @@ inline QueueLess HNSW<Space>::SearchLayer(Point &query, size_t enter_point, size
         }
         for (size_t next : graph_[current].neighbors_[level])
         {
+            if (was_[next] != current_was_)
+            {
+                was_[next] = current_was_;
+                furthest_distance = nearest_neighbours.top().first;
+                FloatType distance = space_.Distance(data_[next], query);
+                if (distance < furthest_distance or nearest_neighbours.size() < ef)
+                {
+                    candidates.emplace(distance, next);
+                    nearest_neighbours.emplace(distance, next);
+                    if (nearest_neighbours.size() > ef)
+                    {
+                        nearest_neighbours.pop();
+                    }
+                }
+            }
+        }
+    }
+    return nearest_neighbours;
+}
+
+template <typename Space>
+inline QueueLess HNSW<Space>::SearchLayer0(Point &query, size_t enter_point, size_t ef, size_t level)
+{
+    was_[enter_point] = ++current_was_;
+    QueueGreater candidates;
+    FloatType enter_point_distance = space_.Distance(data_[enter_point], query);
+    candidates.emplace(enter_point_distance, enter_point);
+    QueueLess nearest_neighbours;
+    nearest_neighbours.emplace(enter_point_distance, enter_point);
+    while (!candidates.empty())
+    {
+        size_t current = candidates.top().second;
+        candidates.pop();
+        FloatType furthest_distance = nearest_neighbours.top().first;
+        if (space_.Distance(data_[current], query) > furthest_distance and
+            nearest_neighbours.size() == ef)
+        {
+            break;
+        }
+        for (size_t i = B0_[2 * current]; i < B0_[2 * current + 1]; ++i)
+        {
+            size_t next = A0_[i];
             if (was_[next] != current_was_)
             {
                 was_[next] = current_was_;
@@ -265,6 +318,160 @@ inline std::vector<size_t> HNSW<Space>::SSG(size_t node, std::vector<size_t> &ca
 }
 
 template <typename Space>
+inline void HNSW<Space>::MemoryManager()
+{
+    std::priority_queue<std::pair<size_t, std::pair<size_t, size_t>>,
+                        std::vector<std::pair<size_t, std::pair<size_t, size_t>>>,
+                        std::less<std::pair<size_t, std::pair<size_t, size_t>>>>
+        q;
+    B0_ = std::vector<size_t>(2 * size_);
+    for (size_t i = 0; i < size_; ++i)
+    {
+        if (i % 10000 == 0)
+        {
+            std::cout << i << "\n";
+        }
+        std::set<int> candidates_;
+        for (auto x : graph_[i].neighbors_[0])
+        {
+            if (i != x)
+            {
+                candidates_.insert(x);
+            }
+            // for (auto y : graph_[x].neighbors_[0])
+            // {
+            //     if (i != y)
+            //     {
+            //         candidates_.insert(y);
+            //     }
+            // }
+        }
+
+        std::set<int> cur;
+        for (auto x : graph_[i].neighbors_[0])
+        {
+            cur.insert(x);
+        }
+        std::set<std::pair<size_t, std::pair<size_t, size_t>>> nq;
+        for (auto x : candidates_)
+        {
+            int count = 0;
+            for (auto y : graph_[x].neighbors_[0])
+            {
+                count += cur.count(y);
+            }
+            nq.insert({count, {i, x}});
+            if (nq.size() > 5)
+            {
+                nq.erase(nq.begin());
+            }
+        }
+        for (auto x : nq)
+        {
+            q.push(x);
+        }
+    }
+    int cnt = 0;
+    int num = 0;
+    std::vector<int> was(size_, -1);
+    while (!q.empty())
+    {
+        if (num >= 200000)
+        {
+            break;
+        }
+        auto t = q.top();
+        q.pop();
+        auto [i, j] = t.second;
+        if (was[i] == -1 and was[j] == -1)
+        {
+            if (i > j)
+            {
+                std::swap(i, j);
+            }
+            was[i] = j;
+            was[j] = j;
+            cnt += t.first;
+            num++;
+        }
+    }
+    std::cout << cnt << "\n";
+    int res = 0;
+    for (int i = 0; i < size_; ++i)
+    {
+        if (was[i] == -1)
+        {
+            AddNeighborhood(i);
+            res++;
+        }
+        else if (was[i] != i)
+        {
+            AddNeighborhood(i, was[i]);
+        }
+    }
+    std::cout << res << "\n";
+}
+
+template <typename Space>
+inline void HNSW<Space>::AddNeighborhood(size_t i, size_t j)
+{
+    std::set<size_t> i_minus_j;
+    std::set<size_t> j_minus_i;
+    std::set<size_t> intersection;
+    for (size_t element : graph_[i].neighbors_[0])
+    {
+        i_minus_j.insert(element);
+    }
+    for (size_t element : graph_[j].neighbors_[0])
+    {
+        if (i_minus_j.count(element))
+        {
+            intersection.insert(element);
+        }
+        else
+        {
+            j_minus_i.insert(element);
+        }
+    }
+    for (size_t element : intersection)
+    {
+        i_minus_j.erase(element);
+    }
+    B0_[2 * i] = size0_;
+    for (auto x : i_minus_j)
+    {
+        A0_.push_back(x);
+        size0_++;
+    }
+    B0_[2 * j] = size0_;
+    for (auto x : intersection)
+    {
+        A0_.push_back(x);
+        size0_++;
+    }
+    B0_[2 * i + 1] = size0_;
+    for (auto x : j_minus_i)
+    {
+        A0_.push_back(x);
+        size0_++;
+    }
+    B0_[2 * j + 1] = size0_;
+}
+
+template <typename Space>
+inline void HNSW<Space>::AddNeighborhood(size_t i)
+{
+    B0_[2 * i] = size0_;
+    for (auto x : graph_[i].neighbors_[0])
+    {
+        A0_.push_back(x);
+        size0_++;
+        // A0_[size0_++] = x;
+    }
+    B0_[2 * i + 1] = size0_;
+}
+
+template <typename Space>
 inline std::vector<size_t> HNSW<Space>::Search(Point &query, size_t K, size_t ef)
 {
     size_t enter_point = enter_point_;
@@ -272,7 +479,7 @@ inline std::vector<size_t> HNSW<Space>::Search(Point &query, size_t K, size_t ef
     {
         enter_point = SearchLayer(query, enter_point, 1, i).top().second;
     }
-    auto nearest_neighbours = SearchLayer(query, enter_point, ef, 0);
+    auto nearest_neighbours = SearchLayer0(query, enter_point, ef, 0);
     while (nearest_neighbours.size() > K)
     {
         nearest_neighbours.pop();
