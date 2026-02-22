e10bc8771769952fe17c39f830f8d8dc14af1b8f
antonov1lya <a222i@ya.ru>
2026-02-22 13:41:56 +0300

    add dist comp option to draw_graphs

--- scripts/draw_graphs.py
+++ scripts/draw_graphs.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def read_file(file):
+def read_file(file, dist_comp):
     x = []
     y = []
     with open(file, 'r') as f:
@@ -13,22 +13,29 @@ def read_file(file):
             arr = i.split()
             if arr[0] == 'recall':
                 x.append(float(arr[1]))
-            if arr[0] == 'time':
-                y.append(1000/(float(arr[1])/1e9))
+            if dist_comp:
+                if arr[0] == 'avg':
+                    y.append(int(arr[3]))
+            else:
+                if arr[0] == 'time':
+                    y.append(1000/(float(arr[1])/1e9))
     return x, y
 
 
-def draw(input_dir: str, output_file: str):
+def draw(input_dir: str, output_file: str, dist_comp):
     directory = Path(input_dir)
     txt_files = list(directory.glob("*.txt"))
     for file in txt_files:
-        x, y = read_file(file)
-        plt.plot(x, y, label=file.stem, marker='^')
+        x, y = read_file(file, dist_comp)
+        plt.plot(x, y, label=file.stem, marker='^', lw=1)
     plt.xticks(np.arange(0.9, 1.0, 0.01))
     plt.legend()
     plt.xlabel('recall')
-    plt.ylabel('qps')
-    plt.grid(True, axis='y')
+    if dist_comp:
+        plt.ylabel('distance computations')
+    else:
+        plt.ylabel('qps')
+    plt.grid(True)
     plt.savefig(output_file, dpi=300, bbox_inches='tight')
 
 
@@ -36,12 +43,13 @@ def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('--input_dir', type=str, required=True)
     parser.add_argument('--output_file', type=str, required=True)
+    parser.add_argument('--dist_comp', action='store_true')
     return parser.parse_args()
 
 
 def main():
     args = parse_arguments()
-    draw(args.input_dir, args.output_file)
+    draw(args.input_dir, args.output_file, args.dist_comp)
 
 
 if __name__ == "__main__":
