b410cec149e62d17aa37b396c31422b7c789af4c
antonov1lya <a222i@ya.ru>
2026-04-26 17:15:11 +0300

    formated code by black

--- scripts/draw_graphs.py
+++ scripts/draw_graphs.py
@@ -8,30 +8,24 @@ import json
 
 
 def draw(json_path: str):
-    with open(json_path, 'r', encoding='utf-8') as file:
+    with open(json_path, "r", encoding="utf-8") as file:
         data_json = json.load(file)
         print(data_json)
         qps_all = []
         configuration = []
-        for file in data_json['data']:
-            data = pd.read_csv(file['file'])
-            data = data.groupby('recall')['time'].min().reset_index()
-            qps = data_json['query'] / (data['time'] / 1e9)
+        for file in data_json["data"]:
+            data = pd.read_csv(file["file"])
+            data = data.groupby("recall")["time"].min().reset_index()
+            qps = data_json["query"] / (data["time"] / 1e9)
             qps_all.append(qps.to_numpy()[-1])
-            configuration.append(file['title'])
-            plt.plot(data['recall'], qps,
-                     label=file['title'], marker='^', lw=1)
+            configuration.append(file["title"])
+            plt.plot(data["recall"], qps, label=file["title"], marker="^", lw=1)
         plt.xticks(np.arange(0.9, 1.0, 0.01))
-        plt.legend(
-            loc='lower center',
-            bbox_to_anchor=(0.5, 1),
-            ncol=2
-        )
-        plt.xlabel('recall@10')
-        plt.ylabel('QPS (1/s)')
-        plt.grid(True, linestyle=':')
-        plt.savefig(data_json['output_file_frontier'],
-                    dpi=300, bbox_inches='tight')
+        plt.legend(loc="lower center", bbox_to_anchor=(0.5, 1), ncol=2)
+        plt.xlabel("recall@10")
+        plt.ylabel("QPS (1/s)")
+        plt.grid(True, linestyle=":")
+        plt.savefig(data_json["output_file_frontier"], dpi=300, bbox_inches="tight")
         plt.clf()
 
         print(qps_all)
@@ -39,7 +33,7 @@ def draw(json_path: str):
             qps_all[i] /= qps_all[0]
         qps_all[0] = 1
 
-        plt.ylabel('Normalized QPS')
+        plt.ylabel("Normalized QPS")
 
         bars = []
         for i, (conf, qps) in enumerate(zip(configuration, qps_all)):
@@ -47,28 +41,25 @@ def draw(json_path: str):
             bars.append(bar[0])
         for bar in bars:
             height = bar.get_height()
-            plt.text(bar.get_x() + bar.get_width()/2., height,
-                     f'{height:.2f}',
-                     ha='center', va='bottom', fontsize=9)
+            plt.text(
+                bar.get_x() + bar.get_width() / 2.0,
+                height,
+                f"{height:.2f}",
+                ha="center",
+                va="bottom",
+                fontsize=9,
+            )
 
         plt.xticks([])
-        plt.xlabel('recall@10=0.99')
-        plt.legend(
-            loc='lower center',
-            bbox_to_anchor=(0.5, 1),
-            ncol=2
-        )
+        plt.xlabel("recall@10=0.99")
+        plt.legend(loc="lower center", bbox_to_anchor=(0.5, 1), ncol=2)
         plt.subplots_adjust(top=0.8)
-        plt.savefig(
-            data_json['output_file_ablation'],
-            dpi=300,
-            bbox_inches='tight'
-        )
+        plt.savefig(data_json["output_file_ablation"], dpi=300, bbox_inches="tight")
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--json-path', type=str, required=True)
+    parser.add_argument("--json-path", type=str, required=True)
     return parser.parse_args()
 
 
--- scripts/train_pq.py
+++ scripts/train_pq.py
@@ -20,29 +20,29 @@ def train_pq(input_path: str, output_dir: str, M: int, angular: bool):
     index.train(train_vectors)
     index.add(train_vectors)
 
-    identity_matrix = np.eye(D, dtype='float32')
+    identity_matrix = np.eye(D, dtype="float32")
     transformed_matrix = opq.apply_py(identity_matrix).T
     PQ = pq.pq
     centroids = faiss.vector_to_array(PQ.centroids)
 
     print(transformed_matrix[0])
-    transformed_matrix.tofile(f'{output_dir}/matrix{M}.bin')
-    centroids.tofile(f'{output_dir}/centroids{M}.bin')
-    faiss.vector_to_array(pq.codes).tofile(f'{output_dir}/data_pq{M}.bin')
+    transformed_matrix.tofile(f"{output_dir}/matrix{M}.bin")
+    centroids.tofile(f"{output_dir}/centroids{M}.bin")
+    faiss.vector_to_array(pq.codes).tofile(f"{output_dir}/data_pq{M}.bin")
 
-    print('l2^2 norm diff:')
+    print("l2^2 norm diff:")
     for i in range(16):
         before = np.linalg.norm(train_vectors[0] - train_vectors[i]) ** 2
         after = np.linalg.norm(train_vectors[0] - index.reconstruct(i)) ** 2
-        print(f'original: {before: .3f}, quantized: {after: .3f}')
+        print(f"original: {before: .3f}, quantized: {after: .3f}")
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input_file', type=str, required=True)
-    parser.add_argument('--output_dir', type=str, required=True)
-    parser.add_argument('--M', type=int, required=True)
-    parser.add_argument('--angular', action='store_true')
+    parser.add_argument("--input_file", type=str, required=True)
+    parser.add_argument("--output_dir", type=str, required=True)
+    parser.add_argument("--M", type=int, required=True)
+    parser.add_argument("--angular", action="store_true")
     return parser.parse_args()
 
 
